f94f1569dec356f9474856c989d3c0044b4ea6b2
Piotr Dobrysiak <p.dobrysiak@multimedia.pl>
2017-02-24 15:20:57 +0100

    Add bower.js

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function (grunt) {
+    //project configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        shell: {
+            multiple: {
+                command: ['bower install',
+                    'mkdir public',
+                    'mv bower_components/** public/',
+                    'rm -rf bower_components'].join('&&')
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-shell');
+
+    //Default Tasks
+    grunt.registerTask('default', ['shell']);
+
+    //production Tasks
+    //grunt.registerTask('dist',[..]);
+
+    //test tasks
+};
