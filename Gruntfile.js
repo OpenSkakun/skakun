a14b4d89f73dace001623241e74191acba04b716
Piotr Dobrysiak <p.dobrysiak@multimedia.pl>
2017-02-28 17:17:42 +0100

    Changes in grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,21 +2,44 @@ module.exports = function (grunt) {
     //project configuration
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
+//            copy: {
+//                dev: {
+//                    files: [
+//                        src: bower_components/
+//                    ]
+//                }
+//            }
         shell: {
             multiple: {
                 command: ['bower install',
                     'rm -r public',
-                    'mkdir public',
-                    'mv bower_components/** public/',
-                    'rm -rf bower_components'].join('&&')
+                    'mkdir public'
+//                    'mv bower_components/** public/'
+//                    'rm -rf bower_components'
+                ].join('&&')
+            }
+        },
+        cssmin: {
+            options: {
+                mergeIntoShorthands: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'output.css': [
+                        'bower_components/bootstrap/dist/css/bootstrap.css'
+                    ]
+                }
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     //Default Tasks
-    grunt.registerTask('default', ['shell']);
+    grunt.registerTask('default', ['shell', 'cssmin']);
 
     //production Tasks
     //grunt.registerTask('dist',[..]);
